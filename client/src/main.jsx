8890019e229d82ebf0a6c1a866afca516fc1802d
alvin-odins <alvin.odins@gmail.com>
2024-08-30 14:06:17 +0300

    feat (routes) set up routes to the admin, booking, home, services, and staff components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,23 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Admin from './pages/admin.jsx'
+import Booking from './pages/Booking.jsx'
+import Home from './pages/home.jsx'
+import Services from './pages/Services.jsx'
+import Staff from './pages/Staff.jsx'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Router>
+    <Routes>
+      <Route path='/' element={<App/>}>
+        <Route index element={<Home/>}/>
+        <Route path="staff" element={<Staff/>}/>
+        <Route path="services" element={<Services/>}/>
+        <Route path="booking" element={<Booking/>}/>
+        <Route path="admin" element={<admin/>}/>
+      </Route>
+    </Routes>
+  </Router>
 )
